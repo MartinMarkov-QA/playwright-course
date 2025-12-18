4bbb8bb593b1345f8cc0ec9f0c9240f941743dc7
Martin Markov <martin.markov1980@gmail.com>
2025-12-18 22:12:38 +0000

    Browser and Page contexts tests.

--- tests/UIBasicTests.spec.ts
+++ tests/UIBasicTests.spec.ts
@@ -1,12 +1,16 @@
 import { test } from "@playwright/test";
 
-
-test('First Playwright test', async ({browser}) => {
+// Browser Context test
+test('Browser Context Playwright test', async ({browser}) => {
     // New browser instance where you can pass all the settings like cookies etc
     const browserContext = await browser.newContext();
 
     // New Page or Tab instance
     const pageInstance = await browserContext.newPage(); 
     await pageInstance.goto('https://google.com');
-    
+});
+
+// Page Context test
+test('Page Context Playwright test', async ({page}) => {
+    await page.goto('https://google.com');
 });
