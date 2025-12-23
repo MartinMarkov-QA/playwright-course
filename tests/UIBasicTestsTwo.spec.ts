283435779aec05fa15934dfb3998133f862e4f5e
Martin Markov <martin.markov1980@gmail.com>
2025-12-23 14:54:37 +0000

    Added new file for testing and tested dropDown option.

--- tests/UIBasicTestsTwo.spec.ts
+++ tests/UIBasicTestsTwo.spec.ts
@@ -3,11 +3,11 @@ import { Locator, test } from "@playwright/test";
 
 test('Selecting element from static drop down', async ({ page }) => {
     const loginUrl = 'https://rahulshettyacademy.com/loginpagePractise/';
-    const selectDropDown = page.getByRole('combobox');
+    const dropDown = page.getByRole('combobox');
     
     await page.goto(loginUrl);
 
     // Select dropdown
-    await selectDropDown.selectOption('Consultant');
+    await dropDown.selectOption('Consultant');
     await page.pause();
 })
