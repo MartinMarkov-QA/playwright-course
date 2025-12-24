d2816bf6d98abc43a8c8d1082720b00b07abb6f1
Martin Markov <martin.markov1980@gmail.com>
2025-12-24 11:18:00 +0000

    Two page interaction test.

--- tests/UIBasicTestsTwo.spec.ts
+++ tests/UIBasicTestsTwo.spec.ts
@@ -1,4 +1,5 @@
 import { expect, Locator, test } from "@playwright/test";
+import { log } from "node:console";
 
 
 test('Selecting element from static drop down', async ({ page }) => {
@@ -22,4 +23,39 @@ test('Selecting element from static drop down', async ({ page }) => {
     // Link attributes 
     const topLink = page.getByRole('link', { name: 'Free Access to InterviewQues/ResumeAssistance/Material' });
     await expect(topLink).toHaveAttribute('class', 'blinkingText');
+})
+
+test.only('Two page interaction', async({ browser }) => {
+    // Creating new browser context where you can pass browser settings like cookies etc
+    const context = await browser.newContext();
+    
+    // Creating new page/tab out of the new browser context
+    const pageOne = await context.newPage();
+
+    pageOne.goto('https://rahulshettyacademy.com/loginpagePractise/');
+
+    // Create new page/tab listener before the actual click event
+    const pagePromise = context.waitForEvent('page');
+
+    // Click the link that opens the new page/tab
+    await pageOne.getByRole('link', { name: 'Free Access to InterviewQues/ResumeAssistance/Material' }).click();
+
+    // Create the new/second page instance
+    const pageTwo = await pagePromise;
+
+    // Console the new page/tab title
+    console.log(await pageTwo.title());
+
+    // Second page locator
+    const pageTwoDataLocator: Locator = pageTwo.getByText('Please email us at mentor@rahulshettyacademy.com with below template to receive response', { exact: true });
+
+    // Wait to make sure the data is stored properly
+    await pageTwoDataLocator.waitFor();
+    const pageTwoData = await pageTwoDataLocator.textContent();
+
+    // Getting the need it user name or defaulting to 'default-username'
+    const pageTwoDataUserName = pageTwoData?.split('@')[1].split(' ')[0] ?? 'default-username';
+
+    // Populating the username data from page two to page one
+    await pageOne.getByRole('textbox', { name: 'Username:' }).fill(pageTwoDataUserName);
 })
