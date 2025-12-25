1a4edfaa0b7024893e83a02e93adcbbaada2fe6b
Martin Markov <martin.markov1980@gmail.com>
2025-12-25 08:31:27 +0000

    Removed the Firefox browser from the config for faster CI testing.

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,10 +26,10 @@ const config = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
   ]
 });
 module.exports = config;
