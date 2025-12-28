d9436d292c102d23428aca22d6018e3145f7730f
Martin Markov <martin.markov1980@gmail.com>
2025-12-28 14:43:38 +0000

    Updated the first test step to "Navigate to login page login adn verify you are on home page"

--- tests/UIBasicsE2E.spec.ts
+++ tests/UIBasicsE2E.spec.ts
@@ -1,14 +1,27 @@
 import { expect, Locator, test } from "@playwright/test";
 
-test.describe("Login and find a specific product and click add to basket", () => {
-  test("E2E test login > find product > add product > ", async ({ page }) => {
+// Apply custom config ONLY to tests in this file
+test.use({
+  testIdAttribute: "data-qa", // Now getByTestId() will use data-qa
+});
 
-    await test.step("Navigate to login page and assert the login form", async () => {
+test.describe("Login and find a specific product and click add to basket", () => {
+  test("E2E test login > find product > add product > assert product > pay", async ({ page }) => {
+
+    await test.step("Navigate to login page login adn verify you are on home page", async () => {
       const loginUrl: string = "https://www.automationexercise.com/login";
+      const homePageTitle = 'Automation Exercise';
+      const userForTesting = {
+        userName: "martinmarkov@gmail.com",
+        password: "408812",
+      };
       const _cookiesAgreeBtn: Locator = page.getByRole("button", {
         name: "Consent",
       });
       const _loginForm: Locator = page.locator(".login-form");
+      const _userName: Locator = page.getByTestId("login-email");
+      const _password: Locator = page.getByTestId("login-password");
+      const _loginBtn = page.getByRole('button', { name: 'Login' });
 
       await page.goto(loginUrl);
       await _cookiesAgreeBtn
@@ -17,6 +30,11 @@ test.describe("Login and find a specific product and click add to basket", () =>
           console.log("Consent banner not present – continuing (common in CI)")
         );
       await expect(_loginForm).toBeVisible();
+      await _userName.fill(userForTesting.userName);
+      await _password.fill(userForTesting.password);
+      await _loginBtn.click();
+      expect(page).toHaveTitle(homePageTitle);
     });
+
   });
 });
