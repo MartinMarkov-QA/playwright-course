2eadb97037d4e934e4f60c721e391086bb5ce90f
Martin Markov <martin.markov1980@gmail.com>
2025-12-29 00:08:16 +0000

    Added "Verify the Men Tshirt product is in the basket and proceed to checkout" test

--- tests/UIBasicsE2E.spec.ts
+++ tests/UIBasicsE2E.spec.ts
@@ -8,7 +8,7 @@ test.use({
 test.describe("Login and find a specific product and click add to basket", () => {
   test("E2E test login > find product > add product > assert product > pay", async ({ page }) => {
 
-    await test.step("Navigate to login page login adn verify you are on home page", async () => {
+    await test.step("Navigate to login page login and verify you are on home page", async () => {
       const loginUrl: string = "https://www.automationexercise.com/login";
       const homePageTitle = 'Automation Exercise';
       const userForTesting = {
@@ -35,6 +35,28 @@ test.describe("Login and find a specific product and click add to basket", () =>
       await _loginBtn.click();
       expect(page).toHaveTitle(homePageTitle);
     });
+
+    await test.step("Add 'Men Tshirt' product to basket", async () => {
+      const _featuresItemsSection: Locator = page.locator('.features_items');
+      const _menTshirtProduct: Locator = _featuresItemsSection.locator('.productinfo').filter({ hasText: 'Men Tshirt' });
+      const _menTshirtProductBtn: Locator = _menTshirtProduct.locator('.add-to-cart');
+      const _productAddedModal: Locator = page.getByRole('heading', { name: 'Added!' });
+      const _viewCartModalBtn: Locator = page.getByText('View Cart', { exact: true });
+      
+      await _menTshirtProductBtn.click();
+      await expect(_productAddedModal).toBeVisible();
+      await _viewCartModalBtn.click();
+    });
+
+    await test.step("Verify the 'Men Tshirt' product is in the basket and proceed to checkout", async () => {
+      const _menTshirt: Locator = page.getByRole('link', { name: 'Men Tshirt' });
+      const _checkoutBtn: Locator = page.getByText('Proceed To Checkout', { exact: true });
+      
+      await expect(page).toHaveTitle('Automation Exercise - Checkout');
+      await expect(_menTshirt).toBeVisible();
+      await _checkoutBtn.click();
+    });
+
 
   });
 });
